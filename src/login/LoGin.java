d8e3f71ba420baeb6d79964ac42db56b1effc08f
mengwenbiao <70381923+mengwenbiao@users.noreply.github.com>
2020-09-29 12:14:49 +0800

    04

--- src/login/LoGin.java
+++ src/login/LoGin.java
@@ -4,5 +4,6 @@ public class LoGin {
 	public static void main(String[] args) {
 		System.out.println("hello word");
 		System.out.println("dhusjkscj");
+		System.out.println("sdfsd  sf sdf sd");
 	}
 }
