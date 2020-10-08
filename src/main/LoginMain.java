ccd746c88f547723c21b51207d3465fcfc72a63c
unknown <1870269308@qq.com>
2020-10-09 00:02:01 +0800

    10.8
    main方法

---
+++ src/main/LoginMain.java
@@ -0,0 +1,10 @@
+package main;
+
+import com.demo.ui.login.Login;
+
+public class LoginMain {
+	public static void main(String[] args) {
+		Login login=new Login();
+		login.getFrame().setVisible(true);
+	}
+}
